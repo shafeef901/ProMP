1b18a932ac114106f9d94f618e76447690d8c2d3
Shafeef Omar <shafeef901@gmail.com>
2021-06-13 16:28:09 +0530

    mb-rl code added

---
+++ run_scripts/meta_mb/meta_algos/__init__.py
@@ -0,0 +1,5 @@
+from meta_mb.meta_algos.base import MAMLAlgo
+from meta_mb.meta_algos.ppo_maml import PPOMAML
+from meta_mb.meta_algos.trpo_maml import TRPOMAML
+from meta_mb.meta_algos.vpg_maml import VPGMAML
+from meta_mb.meta_algos.pro_mp import ProMP
--- run_scripts/test_quad_env.py
+++
@@ -1,9 +0,0 @@
-from meta_policy_search.envs.mujoco_envs.metalhead_v1_rand_direc import MetalheadEnvV1RandDirec
-
-
-env = MetalheadEnvV1RandDirec()
-env.reset()
-
-while True:
-	env.step(env.action_space.sample())
-	env.render()
